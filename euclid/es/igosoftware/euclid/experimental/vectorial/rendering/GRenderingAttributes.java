34c192dbaf47e6a239c5489062c386d22292aa47
diegogomezdeck <DiegoGomezDeck@consultar.com>
2010-11-25 13:18:39 +0100

    added equals and hashcode

--- euclid/es/igosoftware/euclid/experimental/vectorial/rendering/GRenderingAttributes.java
+++ euclid/es/igosoftware/euclid/experimental/vectorial/rendering/GRenderingAttributes.java
@@ -44,4 +44,76 @@ public class GRenderingAttributes {
       _borderStroke = (_borderWidth > 0) ? new BasicStroke(borderWidth, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND) : null;
    }
 
+
+   @Override
+   public int hashCode() {
+      final int prime = 31;
+      int result = 1;
+      result = prime * result + ((_borderColor == null) ? 0 : _borderColor.hashCode());
+      result = prime * result + Float.floatToIntBits(_borderWidth);
+      result = prime * result + (_debugLODRendering ? 1231 : 1237);
+      result = prime * result + ((_fillColor == null) ? 0 : _fillColor.hashCode());
+      long temp;
+      temp = Double.doubleToLongBits(_lodMinSize);
+      result = prime * result + (int) (temp ^ (temp >>> 32));
+      result = prime * result + (_renderBounds ? 1231 : 1237);
+      result = prime * result + (_renderLODIgnores ? 1231 : 1237);
+      result = prime * result + _textureHeight;
+      result = prime * result + _textureWidth;
+      return result;
+   }
+
+
+   @Override
+   public boolean equals(final Object obj) {
+      if (this == obj) {
+         return true;
+      }
+      if (obj == null) {
+         return false;
+      }
+      if (getClass() != obj.getClass()) {
+         return false;
+      }
+      final GRenderingAttributes other = (GRenderingAttributes) obj;
+      if (_borderColor == null) {
+         if (other._borderColor != null) {
+            return false;
+         }
+      }
+      else if (!_borderColor.equals(other._borderColor)) {
+         return false;
+      }
+      if (Float.floatToIntBits(_borderWidth) != Float.floatToIntBits(other._borderWidth)) {
+         return false;
+      }
+      if (_debugLODRendering != other._debugLODRendering) {
+         return false;
+      }
+      if (_fillColor == null) {
+         if (other._fillColor != null) {
+            return false;
+         }
+      }
+      else if (!_fillColor.equals(other._fillColor)) {
+         return false;
+      }
+      if (Double.doubleToLongBits(_lodMinSize) != Double.doubleToLongBits(other._lodMinSize)) {
+         return false;
+      }
+      if (_renderBounds != other._renderBounds) {
+         return false;
+      }
+      if (_renderLODIgnores != other._renderLODIgnores) {
+         return false;
+      }
+      if (_textureHeight != other._textureHeight) {
+         return false;
+      }
+      if (_textureWidth != other._textureWidth) {
+         return false;
+      }
+      return true;
+   }
+
 }
