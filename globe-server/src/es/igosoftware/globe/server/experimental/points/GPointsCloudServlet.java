6c42fc22b451919476d4f500d56b2724a6e55589
diegogomezdeck <DiegoGomezDeck@consultar.com>
2011-01-17 20:37:23 +0100

    minor changes

---
+++ globe-server/src/es/igosoftware/globe/server/experimental/points/GPointsCloudServlet.java
@@ -0,0 +1,43 @@
+
+
+package es.igosoftware.globe.server.experimental.points;
+
+import java.io.IOException;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+
+import es.igosoftware.globe.server.experimental.GHttpServlet;
+import es.igosoftware.globe.server.experimental.GRequestType;
+
+
+public class GPointsCloudServlet
+         extends
+            GHttpServlet {
+
+   private static final long serialVersionUID = 1L;
+
+
+   public GPointsCloudServlet() {
+      super();
+   }
+
+
+   @Override
+   protected void doRequest(final HttpServletRequest request,
+                            final HttpServletResponse response,
+                            final GRequestType requestType) throws IOException {
+      final ServletOutputStream os = response.getOutputStream();
+
+      final Gson gson = new Gson();
+
+      final String[] result = new String[] { "Goodmorning Vietnam!", "another string" };
+      os.println(gson.toJson(result));
+
+      //      os.println("Hello");
+   }
+
+}
