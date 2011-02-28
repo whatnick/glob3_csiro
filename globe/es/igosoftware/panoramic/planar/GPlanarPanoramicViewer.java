989ef62c0c86dfa38ce7ff4a16260e7723b88f64
Manuel de la calle <mdelacalle@MacBook-Pro-de-Manuel-de-la-calle.local>
2011-02-28 20:44:55 +0100

    aa

--- globe/es/igosoftware/panoramic/planar/GPlanarPanoramicViewer.java
+++ globe/es/igosoftware/panoramic/planar/GPlanarPanoramicViewer.java
@@ -951,8 +951,8 @@ public class GPlanarPanoramicViewer {
       //       final URL url = new URL("file:///home/dgd/Desktop/PruebaPanoramicas/PLANAR/PANOS/cantabria1.jpg/");
       //       final URL url = new URL("http://localhost/PANOS/cantabria1.jpg/");
       //       final URL url = new URL("http://localhost/PANOS/PlazaSanJorge-Caceres-Espana.jpg/");
-      final URL url = new URL("http://213.165.81.201:8080/gigapixel/Cantabria1/");
-      //      final URL url = new URL("http://213.165.81.201:8080/gigapixel/PlazaSanJorge-Caceres-Espana.jpg/");
+      //final URL url = new URL("http://213.165.81.201:8080/gigapixel/Cantabria1/");
+      final URL url = new URL("http://213.165.81.201:8080/gigapixel/PlazaSanJorge-Caceres-Espana/");
 
       final int workersCount = GConcurrent.AVAILABLE_PROCESSORS * 2;
       final ILoader loader = new GHttpLoader(url, workersCount, true);
