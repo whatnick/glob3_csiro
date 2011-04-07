a65bd4f22130c88ccecc0ee68a08c4ac0ac00bd7
diegogomezdeck <DiegoGomezDeck@consultar.com>
2011-04-07 18:19:58 +0200

    new ICurve3D interface

---
+++ euclid/es/igosoftware/euclid/ICurve3D.java
@@ -0,0 +1,13 @@
+
+
+package es.igosoftware.euclid;
+
+import es.igosoftware.euclid.bounding.GAxisAlignedBox;
+import es.igosoftware.euclid.vector.IVector3;
+
+
+public interface ICurve3D
+         extends
+            ICurve<IVector3, GAxisAlignedBox> {
+
+}
